17537cd00eec217113b82af0163058b2a8742899
safi-okz <safi90655@gmail.com>
2024-01-28 19:23:55 +0500

    reduce code

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -7,7 +7,7 @@ import logo_icon from '@/assets/img/logo-icon.png';
 </script>
 
 <template>
-    <div class="footer_back -mt-[5rem] overflow-hidden pb-12">
+    <div class="footer_back -mt-[2.8rem] overflow-hidden pb-12">
         <img src="../assets/img/footer.svg" class="" alt="">
         <div class="grid grid-lg-cols-2 md:grid-cols-2 sm:grid-cols-1 px-[5rem]">
             <div>
